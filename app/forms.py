7813b76ea6a082b993775aa3f12fd4af9cc42c74
damullings17 <daniellemullings17@gmail.com>
2021-04-24 18:49:18 -0500

    Exercise 2 Completed

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,12 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed, StringField
+from wtforms.validators import InputRequired
 
+class UploadForm(FlaskForm):
+    description = StringField('Username', validators=[InputRequired()])
+
+    photo = FileField("Photo"
+    ,validators=[
+        FileRequired(),
+        FileAllowed(["jpg","png"],"Images only!")
+        ])
--- app/views.py
+++ app/views.py
@@ -6,12 +6,35 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request
-
+from flask import render_template, request, redirect,jsonify, session, abort, send_from_directory
+from werkzeug.utils import secure_filename
+from app.forms import UploadForm
+import os
 ###
 # Routing for your application.
 ###
 
+@app.route('/api/upload',methods=['POST'])
+def upload():
+    form = UploadForm()
+
+    # Validate file upload on submit
+    if request.method == 'POST':
+        
+        if form.validate_on_submit(): # Validate file upload on submit
+            description = form.description.data
+            photo = form.photo.data
+            filename = secure_filename(photo.filename)
+            photo.save(os.path.join(app.config["UPLOAD_FOLDER"],filename))
+            
+            return jsonify({"message": "File Upload Successful",
+                "filename": filename,
+                "description": description})
+        else:
+            error = [{'error_message': i} for i in form_errors(form)]
+        return jsonify({'errors':error})
+
+    return render_template('upload.html',form=form)
 
 # Please create all new routes and view functions above this route.
 # This route is now our catch all route for our VueJS single page
